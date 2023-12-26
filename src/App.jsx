8646fc49ef13004f940f867307c1c0fe367ce20e
longkhan786 <longkhandhareja78@gmail.com>
2023-12-26 20:38:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import { useEffect, useRef, useState } from "react";
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
+
+const storedId = JSON.parse(localStorage.getItem("selectedPlace")) || [];
+const storedPlaces = storedId.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
+function App() {
+  const modal = useRef();
+  const selectedPlace = useRef();
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((postion) => {
+      const currentPlace = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        postion.coords.latitude,
+        postion.coords.longitude
+      );
+      setAvailablePlaces(currentPlace);
+    });
+  }, []);
+
+  function handleStartRemovePlace(id) {
+    modal.current.open();
+    selectedPlace.current = id;
+  }
+
+  function handleStopRemovePlace() {
+    modal.current.close();
+  }
+
+  function handleSelectPlace(id) {
+    setPickedPlaces((prevPickedPlaces) => {
+      if (prevPickedPlaces.some((place) => place.id === id)) {
+        return prevPickedPlaces;
+      }
+      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      return [place, ...prevPickedPlaces];
+    });
+
+    const storedId = JSON.parse(localStorage.getItem("selectedPlace")) || [];
+    if (storedId.indexOf(id) === -1) {
+      localStorage.setItem("selectedPlace", JSON.stringify([id, ...storedId]));
+    }
+  }
+
+  function handleRemovePlace() {
+    setPickedPlaces((prevPickedPlaces) =>
+      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+    );
+    modal.current.close();
+
+    const storedId = JSON.parse(localStorage.getItem("selectedPlace")) || [];
+    localStorage.setItem(
+      "selectedPlace",
+      JSON.stringify(storedId.filter((id) => id !== selectedPlace.current))
+    );
+  }
+
+  return (
+    <>
+      <Modal ref={modal}>
+        <DeleteConfirmation
+          onCancel={handleStopRemovePlace}
+          onConfirm={handleRemovePlace}
+        />
+      </Modal>
+
+      <header>
+        <img src={logoImg} alt="Stylized globe" />
+        <h1>PlacePicker</h1>
+        <p>
+          Create your personal collection of places you would like to visit or
+          you have visited.
+        </p>
+      </header>
+      <main>
+        <Places
+          title="I'd like to visit ..."
+          fallbackText={"Select the places you would like to visit below."}
+          places={pickedPlaces}
+          onSelectPlace={handleStartRemovePlace}
+        />
+        <Places
+          title="Available Places"
+          places={availablePlaces}
+          fallbackText="Finding Your Nearest Place Accourding to Your Location"
+          onSelectPlace={handleSelectPlace}
+        />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Modal.jsx
@@ -0,0 +1,22 @@
+// Modal.jsx
+
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
+
+const Modal = forwardRef(function Modal({ children }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => ({
+    open: () => dialog.current.showModal(),
+    close: () => dialog.current.close(),
+  }));
+
+  return createPortal(
+    <dialog className="modal" ref={dialog}>
+      {children}
+    </dialog>,
+    document.getElementById('modal')
+  );
+});
+
+export default Modal;
